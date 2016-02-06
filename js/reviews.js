4359bfc9dce51418b0f8a6e18edbd8527597e193
IrinaNizova <alisher_inn@mail.ru>
2016-02-06 20:03:02 +0300

    изменения форматирования

--- js/reviews.js
+++ js/reviews.js
@@ -9,25 +9,24 @@
   formFilter.classList.add('invisible');
 
   var template = document.querySelector('#review-template');
-	reviews.forEach(function(item, index, array) {
-	  var element = template.content.children[0].cloneNode(true);
-	  element.querySelector('.review-text').textContent = item['description'];
+  reviews.forEach(function(item) {
+    var element = template.content.children[0].cloneNode(true);
+    element.querySelector('.review-text').textContent = item['description'];
 
-	  var img = new Image(124, 124);
-      img.src = item["author"]["picture"];
-	  img.onload = function() {
-		var rating = element.querySelector('.review-rating');
-		element.removeChild(rating);
-	    element.querySelector('.review-author').style.backgroundImage = 'url(\'' + img.src + '\')';
-	  };
-	  img.onerror = function() {
-	    var reviewArticle = document.querySelector('.review');
-		reviewArticle.classList.add('.review-load-failure');
-	  }
+    var img = new Image(124, 124);
+    img.src = item['author']['picture'];
+    img.onload = function() {
+      var rating = element.querySelector('.review-rating');
+      element.removeChild(rating);
+      element.querySelector('.review-author').style.backgroundImage = 'url(\'' + img.src + '\')';
+    };
+    img.onerror = function() {
+      var reviewArticle = document.querySelector('.review');
+      reviewArticle.classList.add('.review-load-failure');
+    };
 
-	  var list = document.querySelector('.reviews-list');
-	  list.appendChild(element);
-	 });
-
-	formFilter.classList.remove('invisible');
+    var list = document.querySelector('.reviews-list');
+    list.appendChild(element);
+  });
+  formFilter.classList.remove('invisible');
 })();
