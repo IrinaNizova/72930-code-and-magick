c3fbabab39aabafc88087e120ade2313b65bdd16
IrinaNizova <alisher_inn@mail.ru>
2016-02-17 21:34:09 +0300

    вынесение переменных

--- js/review.js
+++ js/review.js
@@ -15,16 +15,18 @@
 
   Review.prototype._data = null;
 
+  var template = document.querySelector('#review-template');
+  var reviewArticle = document.querySelector('.reviews');
+  var starClassName = ['', 'review-rating', 'review-rating-two',
+		'review-rating-three', 'review-rating-four', 'review-rating-five'];
+
   Review.prototype.render = function() {
-    var template = document.querySelector('#review-template');
-    var reviewArticle = document.querySelector('.reviews');
     this.element = 'content' in template ?
       template.content.children[0].cloneNode(true) :
       template.children[0].cloneNode(true);
     this.element = template.content.children[0].cloneNode(true);
     this.element.querySelector('.review-text').textContent = this._data['description'];
     this.rating = this.element.querySelector('.review-rating');
-    var starClassName = ['', 'review-rating', 'review-rating-two', 'review-rating-three', 'review-rating-four', 'review-rating-five'];
     this.rating.classList.add(starClassName[this._data['rating']]);
 
     var img = new Image(124, 124);
