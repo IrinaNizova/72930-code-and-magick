638f79b192c357edef4ae11a22fa182c9dc449eb
IrinaNizova <alisher_inn@mail.ru>
2016-02-17 23:55:55 +0300

    добавлен inherit

---
+++ js/inherit.js
@@ -0,0 +1,12 @@
+/**
+ * Created by ira on 17.02.16.
+ */
+'use strict';
+
+(function() {
+  window.inherit = function(child, parent) {
+    function emptyCtor = {};
+	emptyCtor.prototype = parent.prototype;
+	child.prototype = new emptyCtor();
+  }
+})();
