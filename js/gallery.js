f4334b9bc05207e1854d87538304c7f33db51123
IrinaNizova <alisher_inn@mail.ru>
2016-02-22 16:57:31 +0300

    первый вариант модуля photo

--- js/gallery.js
+++ js/gallery.js
@@ -12,6 +12,17 @@
     this.rightControl = this.element.querySelector('.overlay-gallery-control-right');
     this._onCloseClick = this._onCloseClick.bind(this);
     this._onDocumentKeyDown = this._onDocumentKeyDown.bind(this);
+    this.currentPhotoNumber = 1;
+    this.setCurrentPhotoNumber = this.setCurrentPhotoNumber.bind(this);
+    this.getCurrentPhotoNumber = this.getCurrentPhotoNumber.bind(this);
+  };
+
+  Gallery.prototype.setCurrentPhotoNumber = function(value) {
+    this.currentPhotoNumber = value;
+  };
+
+  Gallery.prototype.getCurrentPhotoNumber = function() {
+    return this.currentPhotoNumber;
   };
 
   Gallery.prototype.show = function() {
@@ -28,12 +39,32 @@
     this._closeButton.removeEventListener('click', this._onCloseClick);
   };
 
-  Gallery.prototype._leftArrow = function() {
+  Gallery.prototype.setPictures = function(arrayPictures) {
+    this.arrayPictures = arrayPictures;
+  };
 
+  var lastPicture = '';
+  Gallery.prototype.setCurrentPicture = function(num) {
+    var preview = document.querySelector('.overlay-gallery-preview');
+    var picture = this.arrayPictures[num];
+    if (lastPicture) {
+      preview.removeChild(lastPicture);
+    }
+    preview.appendChild(picture);
+    document.querySelector('.preview-number-current').textContent = num + 1;
+    document.querySelector('.preview-number-total').textContent = this.arrayPictures.length;
+    this.setCurrentPhotoNumber(num);
+    lastPicture = picture;
+  };
+
+  Gallery.prototype._leftArrow = function() {
+    if (this.getCurrentPhotoNumber() > 1) {
+      this.setCurrentPicture(this.getCurrentPhotoNumber() - 1);
+    }
   };
 
   Gallery.prototype._rightArrow = function() {
-
+    this.setCurrentPicture(this.getCurrentPhotoNumber() + 1);
   };
 
   Gallery.prototype._onCloseClick = function() {
@@ -44,6 +75,12 @@
   Gallery.prototype._onDocumentKeyDown = function(event) {
     if (event.keyCode === 27) {
       this.hide();
+    }
+    if (event.keyCode === 37) {
+      this._leftArrow();
+    }
+    if (event.keyCode === 39) {
+      this._rightArrow();
     }
   };
 
